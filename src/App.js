50dc0bbec5e2f64b38ac220ed8521a17cff1f0e4
Jessica Latorre <jessica.latorre182@gmail.com>
2021-05-17 17:38:40 -0300

    inicio do projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
+
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Filtros</h3>
+      <label>Valor mínimo:</label>
+      <input type="number"></input>
+      <label>Valor máximo:</label>
+      <input type="number"></input>
+      <label>Busca por nome:</label>
     </div>
-  );
+  )}
 }
 
 export default App;
