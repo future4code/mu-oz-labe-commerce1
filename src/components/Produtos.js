aaa2eded568ce0fc87a6699f0de71460c49201b9
KelvinSaFerreira <kelvin-sa@live.com>
2021-05-20 12:34:43 -0300

    Adição função addToCart

--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -2,19 +2,11 @@ import React from 'react';
 import CardProdutos from './CardProdutos'
 
 export default class Produtos extends React.Component {
-
     render(){
 
         return (
             <div>
-                 {this.props.produtos.map((produto) => {
-                     return (
-                     <CardProdutos 
-                     imageUrl={produto.imageUrl}
-                     name= {produto.name}
-                     value= {produto.value}
-                     adicionarAoCarrinho= {this.props.adicionarAoCarrinho}
-                     />)})}
+                {this.props.cardProdutos}
             </div>
         )
     }
