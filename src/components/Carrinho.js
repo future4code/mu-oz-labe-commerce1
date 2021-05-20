674a46c4a408cb2790e2b4e7a2667a4150ab2d51
KelvinSaFerreira <kelvin-sa@live.com>
2021-05-19 21:33:51 -0300

    Projeto labecommerce 02

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const CarrinhoDiv = styled.div`
+    border: 1px solid black;
+    height: 25px;
+`
 
 export default class Carrinho extends React.Component {
     render(){
         return (
-            <div>
-              
-            </div>
+            <CarrinhoDiv>
+              {this.props.itemCarrinho.map((item) => {
+                  return <p>{item.name}</p>
+              })}
+            </CarrinhoDiv>
         )
     }
 }
