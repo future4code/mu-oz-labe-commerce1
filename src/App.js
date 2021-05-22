0460b2b9cb1b49b81a6cbbdb97fdd86595f5c84a
KelvinSaFerreira <kelvin-sa@live.com>
2021-05-22 15:35:49 -0300

    Adição carrinho funcional

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Filtros from './components/Filtros.js'
 import Produtos from './components/Produtos.js'
 import Carrinho from './components/Carrinho'
 import CardProdutos from './components/CardProdutos'
+import ListaCompras from './components/ListaCompras'
 
 
 export default class App extends React.Component {
@@ -15,45 +16,87 @@ export default class App extends React.Component {
         id: 1,
         name: "Nave Espacial",
         value: 50.00,
-        imageUrl: "https://picsum.photos/200/200"
+        imageUrl: "https://picsum.photos/200/200",
       },
       {
         id: 2,
         name: "Foguete da Missão Apollo 11",
         value: 10000.0,
-        imageUrl: "https://picsum.photos/200/200"
+        imageUrl: "https://picsum.photos/200/200",
       },
       {
         id: 3,
         name: "Ônibus Espacial",
         value: 200.00,
-        imageUrl: "https://picsum.photos/200/200"
+        imageUrl: "https://picsum.photos/200/200",
       }
     ],
-    lista: [
-      // {
-      //   id: "",
-      //   name: "",
-      //   value: "",
-      //   imageUrl: ""
-      // }
-    ]
+    lista: []
   }
 
   adicionarProduto = (id) => {
+    const listaCarrinho = [...this.state.lista]
+
     const novaListaCarrinho = this.state.produtos.filter((produto) => {
       if (id === produto.id) {
         return true
-      } 
+      }
     })
 
-    const listaCarrinho = [...this.state.lista]
-    listaCarrinho.push(...novaListaCarrinho)
+    const checagem = this.state.lista.find((produtoCarrinho) => {
+      if (produtoCarrinho.id === id) {
+        return true
+      } else { return false }
+    })
 
-    this.setState({ lista: listaCarrinho })
+    const produtoSeraAdicionado = {
+      ...novaListaCarrinho[0],
+      quantidade: 1
+    }
+
+    if (!checagem) {
+      listaCarrinho.push(produtoSeraAdicionado)
+
+      this.setState({ lista: listaCarrinho })
+    } else {
+      const adicionarQuantidade = this.state.lista.map((produtoJaAdicionado) => {
+        if (produtoJaAdicionado.id === id) {
+          return {
+            ...produtoJaAdicionado,
+            quantidade: produtoJaAdicionado.quantidade + 1
+          }
+        } else {
+          return produtoJaAdicionado
+        }
+      })
+      this.setState({ lista: adicionarQuantidade })
+    }
+
+
+  }
+
+  removerProdutoCarrinho = (id) => {
+    const novaListaCompras = this.state.lista.map((produto) => {
+      if (produto.id === id) {
+        const produtoAtt = {
+          ...produto,
+          quantidade: produto.quantidade - 1
+        }
+        return produtoAtt
+      } else {return produto}
+    })
+
+    const novaListaComprasAtt = novaListaCompras.filter((produto) => {
+      if (produto && produto.quantidade !== 0){
+        return true
+      }
+    })
+
+    this.setState({ lista: novaListaComprasAtt })
   }
 
   render() {
+
     return (
       <div className="App">
         <h1>Galaxy Kids </h1>
@@ -65,26 +108,23 @@ export default class App extends React.Component {
                 imageUrl={produto.imageUrl}
                 name={produto.name}
                 value={produto.value}
-                adicionarAoCarrinho={() => {this.adicionarProduto(produto.id)}}
+                adicionarAoCarrinho={() => { this.adicionarProduto(produto.id) }}
               />)
           })}
-          // adicionarAoCarrinho={() => this.adicionarProduto()}
         />
         <Carrinho
           itemCarrinho={this.state.lista}
+          listaCompras={this.state.lista.map((produto) => {
+            return (
+              <ListaCompras
+                quantidade={produto.quantidade}
+                name={produto.name}
+                removerProdutoCarrinho={() => { this.removerProdutoCarrinho(produto.id) }}
+              />)
+          })}
+
         />
       </div>
     )
   }
 }
-
-    //   let novoProduto
-  //   for(let index = 0; index < this.state.produtos.length; index++) {
-  //     novoProduto = {
-  //      id: this.state.produtos[index].id,
-  //      name: this.state.produtos[index].name,
-  //      value:this.state.produtos[index].value,
-  //      imageUrl: this.state.produtos[index].imageUrl
-  //  };
-  //  console.log(novoProduto)
-  // }
