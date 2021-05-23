1b7478dd549e1bae6433aaddfd260fc28049f517
Jessica Latorre <jessica.latorre182@gmail.com>
2021-05-22 21:00:41 -0300

    apaguei uma variavel que eu criei para fazer um teste

--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -6,24 +6,7 @@ export default class Produtos extends React.Component {
     
    
     render(){
-        const produtos = [{
-            id: 1,
-            name: "Nave Espacial",
-            value: 50.00,
-            imageUrl: "https://picsum.photos/200/200"
-          },
-        {
-          id: 2,
-              name: "Foguete da Missão Apollo 11",
-              value: 10000.0,
-              imageUrl: "https://picsum.photos/200/200"
-        },
-        {
-          id: 3,
-              name: "Ônibus Espacial",
-              value: 200.00,
-              imageUrl: "https://picsum.photos/200/200"
-        }]
+       
        
 
         return (
